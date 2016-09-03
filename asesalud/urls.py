45da1d23c2be964a8e41b9ed4bff796301191eb4
Josmer Hernandez <jhernandez.18p@gmail.com>
2016-09-03 13:07:49 -0500

    asesalud

--- asesalud/urls.py
+++ asesalud/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from local_apps.frontend import views as front_views
+
 urlpatterns = [
     url(r'^$', front_views.home, name="home"),
+    url(r'^contact$', front_views.contact, name="contact"),
     url(r'^admin/', admin.site.urls),
 ]
--- local_apps/frontend/views.py
+++ local_apps/frontend/views.py
@@ -1,4 +1,6 @@
+from django.core.mail import send_mail
 from django.shortcuts import render
+from decouple import config
 
 # Create your views here.
 def home(request):
@@ -6,3 +8,40 @@ def home(request):
         'title':'Home',
     }
     return render(request, 'frontend/index.html' ,context )
+
+def contact(request):
+
+    if request.method == 'GET':
+
+        return render(request, 'frontend/index.html',{'title':'Home','error':[{'title':'Mensaje vacío.','content':'Ud. debe ingresar contenido en el formulario.'}]})
+
+    elif request.method == 'POST':
+
+        name = request.POST['name']
+        email = request.POST['email']
+        comments = request.POST['comments']
+
+        if name == '':
+
+            return render(request, 'frontend/index.html',{'title':'Home'})
+
+        elif email == '':
+
+            return render(request, 'frontend/index.html',{'title':'Home'})
+
+        elif comments == '':
+
+            return render(request, 'frontend/index.html',{'title':'Home'})
+
+        send_mail(
+		            'Email de contacto, página web',
+		            '%s, ha estado visitando la página web. Su email es: %s, nos ha dejado el siguiente mensaje. \n %s' % (name,email,comments) ,
+		            config("ASESALUD_EMAIL_HOST_USER",),
+		            [config("ASESALUD_EMAIL_HOST_USER",)],
+		            fail_silently=False,
+		        )
+
+        context = {
+            'title': 'Home'
+        }
+        return render(request, 'frontend/index.html', context)
