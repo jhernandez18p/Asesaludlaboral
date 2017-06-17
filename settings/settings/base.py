60e27a274761520430733d260e58e48d5b4652c9
Josmer Hernández <jhernandez.18p@gmail.com>
2017-06-16 20:47:22 -0500

    fix

--- settings/settings/base.py
+++ settings/settings/base.py
@@ -30,13 +30,11 @@ USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'media'))
-# STATICFILES_DIRS = (
-#     os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'staticfiles')),
-# )
-STATIC_ROOT = os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'staticfiles'))
-# STATIC_ROOT = os.path.abspath(
-#     os.path.join(os.path.join(BASE_DIR,os.pardir),'staticfiles'))
+MEDIA_ROOT = os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'media'))
+STATICFILES_DIRS = (
+    os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'staticfiles')),
+)
+# STATIC_ROOT = os.path.abspath(os.path.join(os.path.join(BASE_DIR, os.pardir), 'staticfiles'))
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
